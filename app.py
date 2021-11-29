47486b4c67249e0d437efe085c0c070899bac7df
Matt Grasberger <grasbergerm@gmail.com>
2021-11-29 13:52:39 +0100

    add circleci config and index.html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, current_app
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return current_app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
