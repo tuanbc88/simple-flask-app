d25de5468cd2a089bf10d3fc3d2009dc668c645d
Matt Grasberger <grasbergerm@gmail.com>
2021-12-06 10:56:09 +0100

    fix pylint issues

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+"""simple website app for CI"""
 from flask import Flask, current_app
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """main route to return index.html"""
     return current_app.send_static_file('index.html')
 
 if __name__ == '__main__':
