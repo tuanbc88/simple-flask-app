98ebc0b15ba184f7275a3e98b661a59b4f3727be
TuanBC-hcmut <95213908+tuanbc88-hcmut@users.noreply.github.com>
2023-09-11 22:20:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     """main route to return index.html"""
+    # hello
     return current_app.send_static_file('index.html')
 
 if __name__ == '__main__':
